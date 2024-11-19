3a806f8ac4e7d98dc5da87d38c4f51197fe94b1c
Het-Ghori <hetghorii1@gmail.com>
2024-11-19 12:30:58 +0530

    initilization project

--- Frontend/src/Routes/AdminRoutes.jsx
+++ Frontend/src/Routes/AdminRoutes.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import Layout from '../admin/component/Layout';
 
 function AdminRoutes(props) {
     return (
-        <div>
+        <Layout>
             
-        </div>
+        </Layout>
     );
 }
 
